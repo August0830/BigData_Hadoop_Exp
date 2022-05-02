3df1bc2fc22a9bc5ec0f1679b84860db1be93be1
LyraC42 <597975901@qq.com>
2022-05-02 18:32:20 +0800

    fix a few bugs

--- exp2/src/main/java/com/hadoop/InvertedIndexCombiner.java
+++ exp2/src/main/java/com/hadoop/InvertedIndexCombiner.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 public class InvertedIndexCombiner extends Reducer<Text, Text, Text, Text>{
-    public void combine(Text key, Iterable<Text> values, Context context)
+    public void reduce(Text key, Iterable<Text> values, Context context)
         throws IOException , InterruptedException
     {
-        int sum = 0;
+        int sum = 0;  
         for(Text val : values)
             sum += Integer.parseInt(val.toString());
         String result=Integer.toString(sum);
