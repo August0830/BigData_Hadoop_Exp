c8ac863fdbd57cca0d755ddc074955518c833f1f
August0830 <mzl0830@sina.com>
2022-05-02 20:25:06 +0800

    remove prefix of filename

--- exp2/src/main/java/com/hadoop/InvertedIndexMapper.java
+++ exp2/src/main/java/com/hadoop/InvertedIndexMapper.java
@@ -10,12 +10,16 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 public class InvertedIndexMapper extends Mapper<Object,Text,Text,Text>  {
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         FileSplit split = (FileSplit)context.getInputSplit();
+        String reversePath = new StringBuilder(split.getPath().toString()).reverse().toString();
+        int splitIndex = reversePath.indexOf("/");
+        String path = new StringBuilder(reversePath.substring(0,splitIndex)).reverse().toString();
         StringTokenizer str = new StringTokenizer(value.toString());
         Text keyInfo = new Text();
         Text valueInfo = new Text();
         while(str.hasMoreTokens()){
             //how to move prefix of filename
-            keyInfo.set(str.nextToken()+","+split.getPath().toString());
+            keyInfo.set(str.nextToken()+","+path);
+            //keyInfo.set(str.nextToken()+","+split.getPath().toString());
             valueInfo.set("1");
             context.write(keyInfo,valueInfo);
         }
